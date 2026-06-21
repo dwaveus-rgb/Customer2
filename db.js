8128b2b55cb12d0ec40526b72330aff72299288f
dwaveus-rgb <dwaveus@gmail.com>
2026-06-21 23:06:02 +0500

    fix: simplify db.js fallback, remove broken testConnection call

--- db.js
+++ db.js
@@ -1,21 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-
 const usePostgres = !!process.env.DATABASE_URL;
 
 if (usePostgres) {
   try {
-    const pgdb = require('./db-postgres');
-    pgdb.testConnection().then(ok => {
-      if (ok) {
-        console.log('[DB] Using PostgreSQL');
-      } else {
-        throw new Error('Connection failed');
-      }
-    }).catch(() => {
-      console.log('[DB] PostgreSQL unreachable, falling back to JSON');
-    });
-    module.exports = pgdb;
+    console.log('[DB] Using PostgreSQL');
+    module.exports = require('./db-postgres');
   } catch (e) {
     console.log('[DB] PostgreSQL failed to load, using local JSON');
     module.exports = require('./db-json');
